6ad62a60f0e060a1fe7c55cd509cc63a705656b9
Yash-Salokhe-UofA <yashsalokhe@arizona.edu>
2025-10-24 20:40:34 -0700

    added solution for RightAngledTriangle

---
+++ RightAngledTriangle.py
@@ -0,0 +1,14 @@
+def generate_triangle(n):
+    """
+    Function to return a right-angled triangle of '*' of side n as a list of strings.
+    
+    Parameters:
+    n (int): The height and base of the triangle.
+    
+    Returns:
+    list: A list of strings where each string represents a row of the triangle.
+    """
+    triangle = []
+    for i in range(n):
+        triangle.append('*'*(i+1))
+    return triangle
