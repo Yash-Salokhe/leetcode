a5d71cd1302722c3cbbd2fab4a87367b53be3191
Yash-Salokhe-UofA <yashsalokhe@arizona.edu>
2025-10-27 12:05:36 -0700

    added solution to RAT_II

---
+++ RAT_II.py
@@ -0,0 +1,17 @@
+def generate_right_angled_triangle(n):
+    """
+    Function to return a right-angled triangle of '*' of side n as a list of strings.
+    
+    Parameters:
+    n (int): The height of the triangle.
+    
+    Returns:
+    list: A list of strings where each string represents a row of the triangle.
+    """
+
+    return [" "*(n-i) + "*"*i for i in range(1, n+1)]
+    
+    # Alternative solution
+    # result = []
+    # result.append(" "*(n-i) + "*"*i for i in range(1,n+1))
+    # return result
